6249937c8221862a3024f91b6a06d947f1352176
Christian Torres <chtorrez@gmail.com>
2013-06-03 19:37:05 -0600

    Agregando south para migración de la BD.

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -124,6 +124,9 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
 
+    # Third-Party Apps
+    'south',
+
     # App Core
     'core',
 )
