fbed7c1ab1fb3fa1d7948f5a7252d4c1714b19a8
Christian Torres <chtorrez@gmail.com>
2013-06-10 15:19:46 -0600

    robots.txt

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.static import static
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,6 +17,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    (r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 )
 
 if settings.DEBUG and settings.MEDIA_ROOT:
